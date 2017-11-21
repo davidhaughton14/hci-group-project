456d594415589304978c76d7c54a214e589b7c8f
davidhaughton14 <2080734h@student.gla.ac.uk>
2017-11-21 14:01:08 +0000

    Updated colours of counter values slightly

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -11,8 +11,8 @@ function fillSteps(){
     var percentLeft = stepsDifference/10000;
     var fillValue = (400-(percentLeft*400));
     var grd=context.createLinearGradient(0,0,fillValue,0);
-    grd.addColorStop(0,"white");
-    grd.addColorStop(1,"green");
+    grd.addColorStop(0,"#DCEABE");
+    grd.addColorStop(1,"#95d10a");
     context.beginPath();
     context.rect(0, 0, fillValue, 40);
     context.fillStyle = grd;
@@ -27,8 +27,8 @@ function fillSleep(){
     var percentLeft = stepsDifference/8;
     var fillValue = (400-(percentLeft*400));
     var grd=context.createLinearGradient(0,0,fillValue,0);
-    grd.addColorStop(0,"white");
-    grd.addColorStop(1,"green");
+    grd.addColorStop(0,"#DCEABE");
+    grd.addColorStop(1,"#95d10a");
     context.beginPath();
     context.rect(0, 0, fillValue, 40);
     context.fillStyle = grd;
