dad6e08c1a8ed2b0b4285993f11cb578d895f2d2
davidhaughton14 <2080734h@student.gla.ac.uk>
2017-11-21 10:52:17 +0000

    Added logic to fill step progress bar

---
+++ public/javascripts/dashboard.js
@@ -0,0 +1,11 @@
+var canvas = document.getElementById('steps-progress');
+var context = canvas.getContext('2d');
+var steps = 7342;
+var stepsDifference = (10000-steps);
+var percentLeft = stepsDifference/10000;
+var fillValue = (400-(percentLeft*400));
+context.beginPath();
+context.rect(0, 0, fillValue, 40);
+context.fillStyle = 'green';
+context.fill();
+context.noStroke();
