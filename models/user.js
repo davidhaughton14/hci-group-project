3cd953826ad027c7e756740d87d652b813446c12
davidhaughton14 <2080734h@student.gla.ac.uk>
2017-11-23 15:56:42 +0000

    New schema fully working

--- models/user.js
+++ models/user.js
@@ -1,8 +1,42 @@
 var mongoose = require('mongoose');
 var bcrypt = require('bcryptjs');
+const Schema = mongoose.Schema;
+
+const DiarySchema = mongoose.Schema({
+	date:{
+		type:Number
+	},
+	rating:{
+		type:Number
+	},
+	text:{
+		type:String
+	},
+	_id:false
+});
+
+const HabitDaySchema = mongoose.Schema({
+	 date: {
+		type:Number
+	},
+	value: {
+		type: String
+	},
+	_id: false
+});
+
+const HabitSchema = new Schema({
+    name: {
+    	type: String
+	},
+	day: {
+		type: [HabitDaySchema]
+	},
+	_id: false
+});
 
 // User Schema
-var UserSchema = mongoose.Schema({
+const UserSchema = new Schema({
 	username: {
 		type: String,
 		index:true
@@ -10,19 +44,14 @@ var UserSchema = mongoose.Schema({
 	password: {
 		type: String
 	},
-
-	habits: [{
-		id: {
-			type: Number,
-			required: true
-		},
-		name: {
-			type: String
-		}
-	}]
+	habits:[String],
+	tracked_stats: [HabitSchema],
+	diaryEntries: [DiarySchema]
 });
 
-var User = module.exports = mongoose.model('User', UserSchema);
+var User = mongoose.model('User', UserSchema);
+
+module.exports = User;
 
 module.exports.createUser = function(newUser, callback){
 	bcrypt.genSalt(10, function(err, salt) {
