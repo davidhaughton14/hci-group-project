0144d2a5292c0293465da3db29cc997f47438555
davidhaughton14 <2080734h@student.gla.ac.uk>
2017-11-26 16:37:00 +0000

    Added habit circles to helper page

--- public/javascripts/view-user.js
+++ public/javascripts/view-user.js
@@ -1,9 +1,10 @@
 $('#modalDIARY').on('show.bs.modal', function (e) {
     var date = $(e.relatedTarget).data('diary-date');
     var diarytext = $(e.relatedTarget).data('diary-text');
-    var diaryRating = $(e.relatedTarget).data('diary-rating');
+    var rating = $(e.relatedTarget).data('diary-rating');
 
     var diaryHeading = "Diary entry for "+date;
+    var diaryRating = rating + "/10";
     $("#diaryDate").html(diaryHeading);
     $("#diaryText").html(diarytext);
     $("#diaryRating").html(diaryRating);
