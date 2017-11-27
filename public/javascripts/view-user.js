a5f3070a6b86a4721c072492164baf931919c59a
maximilian <2085099m@student.gla.ac.uk>
2017-11-27 18:35:36 +0000

    styling

--- public/javascripts/view-user.js
+++ public/javascripts/view-user.js
@@ -24,17 +24,30 @@ $('#habitMODAL').on('show.bs.modal', function (e) {
 	$("#general_div").hide();
 	$("#calendar_general_div").hide();
 	
+	$("#bloodtitle").html("");
+	$("#heartratetitle").html("");
+	$("#sleeptitle").html("");
+	$("#vitdtitle").html("");
+	$("#sleeptitle").html("");
+	$("#generaltitle").html("");
 
    	if (name == "BloodPressure") {
+   		$("#bloodtitle").html("Users average daily blood pressure vs daily heart rate - past 30 days");
     	$("#blood_div").show();
+
+    	$("#heartratetitle").html("Users average daily heart rate - past 30 days");
     	$("#heartrate_div").show();
 	} else if (name == "Sleep"){
+		$("#sleeptitle").html("Breakdown of users sleep vs daily mood - past 7 days");
 		$("#sleep_div").show();
 	} else if (name == "VitaminD"){
+		$("#vitdtitle").html("Vitamin D levels vs daily mood - past 7 days");
 		$("#vitd_div").show();
 	} else if (name == "StepCount"){
+		$("#sleeptitle").html("Number of daily steps - past 7 days");
 		$("#steps_div").show();
 	} else {
+		$("#generaltitle").html("Daily tracked values for habit vs daily mood - past 7 days");
 		$("#general_div").show();
 		$("#calendar_general_div").show();
 	}
